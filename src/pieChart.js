98b6f445d6c9978743759eca650e20abd03195f3
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-10 10:09:34 +1200

    comments in pieChart

--- src/pieChart.js
+++ src/pieChart.js
@@ -2,21 +2,21 @@ var d3 = require('d3')
 
 
 module.exports = function(data, root) {
-  var color = d3.scale.category20b()
+  var color = d3.scale.category20b() // sets the color palette
 
-  var arc = d3.svg.arc()
+  var svg = d3.select(root).append('svg') // select root element (div where it will append), append svg element
+      .attr("width", 400)
+      .attr("height", 400) // sets width/height of svg element
+      .append("g")
+      .attr("transform", "translate(" + 200 + "," + 200 + ")"); // translates (shifts) chart (g element) to middle of svg element
+
+  var arc = d3.svg.arc() // defines the radius of the chart
       .outerRadius(200 - 10)
-      .innerRadius(90);
+      .innerRadius(90); // anything greater than 0 will make it a 'donut' chart
 
   var pie = d3.layout.pie()
-      .sort(null)
-      .value(function(d) { return d.count; });
-
-  var svg = d3.select(root).append('svg')
-      .attr("width", 400)
-      .attr("height", 400)
-      .append("g")
-      .attr("transform", "translate(" + 200 + "," + 200 + ")");
+      .sort(null) // disable sorting the data ( it is already sorted )
+      .value(function(d) { return d.count; }); // this section defines the start & end angles of the segments
 
     var g = svg.selectAll(".arc")
         .data(pie(data))
