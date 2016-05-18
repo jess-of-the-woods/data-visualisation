1999713c9a5aac464eabd2752475747560c0ea69
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-18 16:47:57 +1200

    add script to start server, uncomment code in index, not talkin 2 server

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Form from './form'
 import TweetContainer from './tweetContainer'
+import HashtagAssociates from './hashtagAssociates'
 
 class App extends React.Component{
   constructor(props){
@@ -13,6 +14,7 @@ class App extends React.Component{
         <h1>Hashtag Web</h1>
         <Form />
         <TweetContainer />
+        <HashtagAssociates />
       </div>
     )
   }
---
+++ src/components/hashtagAssociates.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class HashtagAssociates extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div id='hashtagAssociatesDiv' className="container">
+    		<h5 className="hashtagsHeader">Associated Hashtags:</h5>
+    		<h6 className="hashtagsHeader">Click on a hashtag in the chart to search for it</h6>
+    		<div id='pieChart'></div>
+    		<p id='hashtagAssociates'></p>
+    	</div>
+    )
+  }
+}
+
+export default HashtagAssociates
